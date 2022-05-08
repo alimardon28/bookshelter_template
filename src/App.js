d07501e711e4ccb8b8a34624b0084c70ff705baf
alimardon28 <doniyorovalimardon369@gmail.com>
2022-05-08 12:53:38 +0500

    finished bookelements pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import BookElements from './Pages/BookElements/BookElements';
 import Bookmark from './Pages/Bookmark/Bookmark';
-import BookElements from './Pages/BookElements/BookElements'
 
 function App() {
   return (
--- src/Pages/BookElements/BookElements.jsx
+++ src/Pages/BookElements/BookElements.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
+import img from "../../Components/assets/img/images.png";
+import "../BookElements/BookElements.scss";
 
 const BookElements = () => {
-    return (
-        <div>
-            <h1>Book Elements</h1>
+  return (
+    <>
+      <div className="bookmarkElements">
+        <div className="container">
+          <ul className="bookmarkElements__list">
+            <li className="bookmarkElements__list-item">
+              <div className="bookmarkElements__list-item_images">
+                <img src={img} alt="images" />
+              </div>
+              <div className="bookmarkElements__list-item_element">
+                <h3 className="bookmarkElements__list-item_element-title">
+                  Python
+                </h3>
+                <p className="bookmarkElements__list-item_element-desc">
+                  David M. Beazley
+                </p>
+                <span className="bookmarkElements__list-item_element-number">
+                  2009
+                </span>
+              </div>
+              <div className="bookmarkElements__list-item_Buttons">
+                <button className="bookmarkElements__list-item_Buttons-button">
+                  Bookmark
+                </button>
+                <button className="bookmarkElements__list-item_Buttons-button">
+                  More Info
+                </button>
+              </div>
+              <div className="bookmarkElements__list-item_button">
+                <button className="bookmarkElements__list-item_button-btn">
+                  Read
+                </button>
+              </div>
+            </li>
+          </ul>
         </div>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default BookElements;
